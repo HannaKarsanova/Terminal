09bad156237933008b0f117e5d2a18e27eee530e
HannaKarsanova <129978942+HannaKarsanova@users.noreply.github.com>
2023-04-30 11:03:54 +0300

    Add files via upload

---
+++ study.sh
@@ -0,0 +1,11 @@
+!#/bin/bash
+mkdir study
+cd study 
+mkdir dir1 dir2 dir3
+cd dir1
+touch test1.txt test2.txt test3.txt test4.json test5.json
+mkdir dir4 dir5 dir6
+ls -la
+mv test1.txt dir4
+mv test2.txt dir5
+
